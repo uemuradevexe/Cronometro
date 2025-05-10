dbcbd66374f08b2883a84c827b75127e9e75c3b8
Ricardo Uemura <uemuradev.exe@gmail.com>
2025-05-10 20:32:31 +0900

    Adicionar arquivos de projeto.

---
+++ Cronometro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 1000000000;
+            
+            int currentTime = 0;
+
+            while (time != currentTime)
+            {
+                currentTime++;
+                Console.WriteLine(currentTime);
+             }
+        }
+        
+        
+
+    }
+}   
